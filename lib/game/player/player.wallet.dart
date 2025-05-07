d5714bd44868c2e483aa4a579f6984074df4a8ad
Leonardo Lin <leolkw8@gmail.com>
2025-05-07 15:05:33 -0300

    added show wallet

--- lib/game/player/player.wallet.dart
+++ lib/game/player/player.wallet.dart
@@ -19,10 +19,14 @@ void removecoin (int amount){
 }
 }
 class MyWidget extends StatelessWidget {
+  final PlayerWallet wallet;
+
+  MyWidget ({required this.wallet});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(PlayerWallet.toString()),
+      child: Text(wallet.balance.toString()),
     );
   }
 }

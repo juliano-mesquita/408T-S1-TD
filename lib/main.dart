c3cb229deee613f496d21d23f05e3721c12a1199
Juliano Souza <juliano@mesquita.com.br>
2025-05-28 17:01:24 -0300

    Removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_towerdefense_game/controller/player_controller.dart';
 import 'package:flutter_towerdefense_game/game/market_inventory.dart';
-import 'package:flutter_towerdefense_game/game/player/player.dart';
 import 'package:flutter_towerdefense_game/game/tower_defense_game.dart';
 import 'package:flutter_towerdefense_game/provider/player_provider.dart';
 import 'package:flutter_towerdefense_game/repository/player_repository.dart';

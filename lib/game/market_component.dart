5b52b77602200d94648a2971b2395065e1473d92
Juliano Souza <juliano@mesquita.com.br>
2025-06-25 14:37:00 -0300

    fix: Fix market item widget not reacting to player wallet balance

--- lib/game/market_component.dart
+++ lib/game/market_component.dart
@@ -1,17 +1,64 @@
 import 'package:flutter_towerdefense_game/controller/market_inventory_controller.dart';
+import 'package:flutter_towerdefense_game/controller/player_controller.dart';
 
 import 'market_item_widget.dart';
 import 'package:flutter/material.dart';
 
-class MarketComponent extends StatelessWidget
+class MarketComponent extends StatefulWidget
 {
   final MarketInventoryController marketInventoryController;
-  const MarketComponent({super.key, required this.marketInventoryController});
+  final PlayerController playerController;
+
+  const MarketComponent
+  (
+    {
+      super.key,
+      required this.marketInventoryController,
+      required this.playerController
+    }
+  );
+  
+  @override
+  State<StatefulWidget> createState() => _MarketComponentState();
+}
+
+class _MarketComponentState extends State<MarketComponent>
+{
+  late int _playerBalance = widget.playerController.player.wallet.balance;
+
+  @override
+  void initState()
+  {
+    super.initState();
+    widget.playerController.addListener(_onPlayerUpdate);
+  }
+
+  @override
+  void dispose()
+  {
+    widget.playerController.removeListener(_onPlayerUpdate);
+    super.dispose();
+  }
+
+  void _onPlayerUpdate()
+  {
+    final player = widget.playerController.player;
+    if(player.wallet.balance == _playerBalance)
+    {
+      return;
+    }
+    setState(
+      ()
+      {
+        _playerBalance = player.wallet.balance;
+      }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
-      valueListenable: marketInventoryController,
+      valueListenable: widget.marketInventoryController,
       builder: (context, value, child)
       {
         final items = value?.items ?? [];
@@ -25,7 +72,10 @@ class MarketComponent extends StatelessWidget
               scrollDirection: Axis.horizontal,
               itemCount: items.length,
               itemBuilder: (context, index) {
-                return MarketItemWidget(item: items[index]);
+                return MarketItemWidget(
+                  item: items[index],
+                  canAfford: _playerBalance >= items[index].price
+                );
               },
             ),
           ),

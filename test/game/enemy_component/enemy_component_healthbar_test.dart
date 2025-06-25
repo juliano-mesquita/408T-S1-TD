9fb53bf5e1bd406a54a2a1adaa8c5d914375ae89
elderzinho <gabriel_vizorek_cezar@hotmail.com>
2025-06-25 17:21:55 -0300

    new healthbar tests

--- test/game/enemy_component/enemy_component_healthbar_test.dart
+++ test/game/enemy_component/enemy_component_healthbar_test.dart
@@ -1,11 +1,16 @@
+import 'package:flame/flame.dart';
+import 'package:flame_test/flame_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flame/components.dart';
 import 'package:flutter_towerdefense_game/game/component/health_bar_component.dart';
 import 'package:flutter_towerdefense_game/game/enemy_component/enemy_component.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
   group('EnemyComponent HealthBar', () {
-    test('deve conter um HealthBarComponent como filho', () async {
+    testWithFlameGame(
+      'deve conter um HealthBarComponent como filho',
+      (game) async {
       // Dados dummy para instanciar o inimigo
       final path = [
         [1, 1, 0],
@@ -22,17 +27,15 @@ void main() {
         path: path,
         startPos: Vector2(0, 0),
         tiles: tiles,
-      );
-
-      // Como o add(healthBar) é chamado no build(), ele precisa ser montado
-      // no game tree para que o Flame chame o onLoad e o add funcione.
-      // Para isso, criamos um componente pai fictício e o adicionamos:
-      final parent = Component()..add(enemy);
-
-      // Avançamos o game uma vez para que o ciclo de onLoad/onMount ocorra
-      await parent.onLoad();
-
-      // Procura o HealthBarComponent entre os filhos do enemy
+      )
+        ..sprite = Sprite(
+          await Flame.images.load('indios_garimpeiros/garimpeira.png'),  
+        )
+        ..size = Vector2(35,35)
+        ..anchor = Anchor.center;
+    
+      game.add(enemy);
+      await game.ready();
       final healthBar = enemy.children.whereType<HealthBarComponent>().toList();
 
       expect(healthBar.length, 1); // Existe exatamente um HealthBarComponent

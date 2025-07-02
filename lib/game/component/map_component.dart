e065a6c967462bdde95d5d9c296f8a581ed18bfd
Gabriel Cezar <gabriel_vizorek_cezar@hotmail.com>
2025-07-02 16:36:02 -0300

    Enemy Movement & Health bar
    * WIP: Enemy Path Following Algorithm
    
    * WIP: Add basic enemy movement
    
    * Added enemy healhbars
    
    * Added spawn timer and maximum number of enemies
    
    * added spawn timers and maximum number of enemies
    
    * fixed formating
    
    * Added null to nextPos, enemies  now dissapear on reaching the end
    
    * Added player damage. TODO: add  player health bar
    
    * removed print/dart:ui import
    
    * fix: Fixed health bar positioning
    
    * fix: Enemy not spawning in correct Y position
    
    * added tests for enemy component
    
    * new healthbar tests
    
    * added testes for EnemyComponent
    
    * Deleted individual test
    
    * Added enemy speed calculation
    
    * added speed calculation to enemycomponent
    
    ---------
    
    Co-authored-by: Juliano Souza <juliano@mesquita.com.br>

--- lib/game/component/map_component.dart
+++ lib/game/component/map_component.dart
@@ -1,9 +1,11 @@
-import 'package:flame/components.dart';
+import 'dart:async';
+import 'package:flame/components.dart' hide Timer;
 import 'package:flame/effects.dart';
 import 'package:flame/extensions.dart';
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_towerdefense_game/game/component/tile_component.dart';
+import 'package:flutter_towerdefense_game/game/enemy_component/enemy_component.dart';
 import 'package:flutter_towerdefense_game/game/schema/map_object.dart';
 import 'package:flutter_towerdefense_game/game/tower/tower_attributes.dart';
 import 'package:flutter_towerdefense_game/game/tower/tower_component.dart';
@@ -26,29 +28,62 @@ class MapComponent extends PositionComponent {
   late final double _tileSize;
 
   /// A list of tiles generated for the map
-  late final List<SpriteComponent> _tiles = [];
+  late final List<List<SpriteComponent>> _tiles = [];
 
   /// A map that matches tile types to their sprites/images
   late final Map<TileType, Sprite> _tilesToSprite;
 
+  late final List<PositionComponent> _enemies = [];
   final List<Vector2> _validTowerPositions = [];
 
   final Set<Vector2> _occupiedTowerPositions = {};
 
-  late final Sprite towerSprite; 
+  late final Sprite towerSprite;
+  late final Sprite enemySprite;
+  int playerHealth = 5;
 
-  MapComponent(
-    {
-      required this.mapObject
+  int _enemyCount = 0;
+  final int _maxEnemies = 10;
+  Timer? _enemySpawnTimer;
+
+  MapComponent({required this.mapObject});
+
+  /// Finds the first top left road in them map
+  Vector2 findLeftTopMostRoadTile() {
+    //TODO: Optimize to break after finding the first point when looking from top/left to bottom/right
+    final tiles = mapObject.points.map(
+      (row) => row.indexWhere((tile) => tile == TileType.road),
+    );
+    int minY = tiles.length;
+    int minX = tiles.first;
+    for (int y = 0; y < mapObject.height; y++) {
+      for (int x = 0; x < mapObject.width; x++) {
+        if (mapObject.points[y][x] == TileType.road && x <= minX) {
+          minX = x;
+        }
+      }
     }
-  );
+
+    for (int y = 0; y < mapObject.height; y++) {
+      if (mapObject.points[y][minX] == TileType.road && y <= minY) {
+        minY = y;
+      }
+    }
+
+    return Vector2(minX.toDouble(), minY.toDouble());
+  }
 
   @override
   Future<void> onLoad() async {
     /// Intiliaze sprite images and variables
     await _initializeSprites();
 
-    towerSprite = Sprite(await Flame.images.load('indios_garimpeiros/indio_um.png'));
+    towerSprite = Sprite(
+      await Flame.images.load('indios_garimpeiros/indio_um.png'),
+    );
+    enemySprite = Sprite(
+      await Flame.images.load('indios_garimpeiros/garimpeira.png'),
+    );
 
     /// Set map scaled size
     _setMapScaledSize();
@@ -56,7 +91,69 @@ class MapComponent extends PositionComponent {
     /// Generate map
     await _generateMap();
 
+    /// Adds an enemy every two seconds
+    //TODO: Use correctly rules to add an enemy
+    _enemySpawnTimer = Timer.periodic(const Duration(seconds: 2), (_) {
+      if (_enemyCount < _maxEnemies) {
+        _addEnemy();
+      } else {
+        _enemySpawnTimer?.cancel();
+      }
+    });
+
     await super.onLoad();
+  }
+
+  /// Adds an enemy to the map
+  void _addEnemy() {
+    if (_enemyCount >= _maxEnemies) return;
+    final firstRoadTilePos = findLeftTopMostRoadTile();
+    var firstRoadTile =
+        _tiles[firstRoadTilePos.y.toInt()][firstRoadTilePos.x.toInt()];
+    final List<List<int>> enemyPath = [];
+    // Generates the enemy path
+    for (int y = 0; y < _tiles.length; y++) {
+      final List<int> column = [];
+      for (int x = 0; x < _tiles[y].length; x++) {
+        final tileInfo = _getTileInfo(x, y);
+        switch (tileInfo.type) {
+          case TileType.grass:
+            column.add(-1);
+            break;
+          case TileType.road:
+            column.add(1);
+            break;
+          case TileType.roadTurn:
+            column.add(2);
+            break;
+          case TileType.roadJunction:
+            column.add(3);
+            break;
+        }
+      }
+      enemyPath.add(column);
+    }
+    final enemySize = Vector2(35, 35);
+    final enemy =
+        EnemyComponent.build(
+            type: EnemyType.type1,
+            path: enemyPath,
+            startPos: firstRoadTilePos,
+            tiles: _tiles,
+            onReachedEnd: () {
+              playerHealth -= 1;
+            },
+          )
+          ..sprite = enemySprite
+          ..size = enemySize
+          ..anchor = Anchor.center
+          ..position = Vector2(
+            firstRoadTile.absoluteCenter.x - (enemySize.x/2),
+            firstRoadTile.absoluteCenter.y
+          );
+    add(enemy);
+    _enemies.add(enemy);
+    _enemyCount++;
   }
 
   /// Calculates and scale map tile sizes
@@ -92,8 +189,9 @@ class MapComponent extends PositionComponent {
   /// Generates the map based on provided [mapObject] and
   /// scaled viewport sizes
   Future<void> _generateMap() async {
-    for (int tileIndexX = 0; tileIndexX < mapObject.width; tileIndexX++) {
-      for (int tileIndexY = 0; tileIndexY < mapObject.height; tileIndexY++) {
+    for (int tileIndexY = 0; tileIndexY < mapObject.height; tileIndexY++) {
+      final List<SpriteComponent> tileRow = [];
+      for (int tileIndexX = 0; tileIndexX < mapObject.width; tileIndexX++) {
         var tileInfo = _getTileInfo(tileIndexX, tileIndexY);
 
         final left = (tileIndexX * _tileSize) + (_tileSize / 2);
@@ -101,23 +199,27 @@ class MapComponent extends PositionComponent {
 
         final rotationAngle = tileInfo.rotationAngle * (math.pi / 180);
 
-        final tile = TileComponent(
-          xMap: tileIndexX,
-          yMap: tileIndexY,
-          onTapDownCallback: handleTap
-        )
-          ..anchor=Anchor.center
-          ..size=Vector2(_tileSize, _tileSize)
-          ..position=Vector2(left, top)
-          ..angle=rotationAngle;
+        final tile =
+            TileComponent(
+                xMap: tileIndexX,
+                yMap: tileIndexY,
+                onTapDownCallback: handleTap,
+              )
+              ..anchor = Anchor.center
+              ..size = Vector2(_tileSize, _tileSize)
+              ..position = Vector2(left, top)
+              ..angle = rotationAngle;
         tile.sprite = tileInfo.sprite;
 
-        _tiles.add(tile);
+        tileRow.add(tile);
         add(tile);
         if (tileInfo.type == TileType.grass) {
-          _validTowerPositions.add(Vector2(tileIndexX.toDouble(), tileIndexY.toDouble()));
+          _validTowerPositions.add(
+            Vector2(tileIndexX.toDouble(), tileIndexY.toDouble()),
+          );
         }
       }
+      _tiles.add(tileRow);
     }
   }
 
@@ -162,6 +264,7 @@ class MapComponent extends PositionComponent {
 
     // Load the sprite for the current tile
     Sprite sprite = _tilesToSprite[mapTileType]!;
+    TileType tileType = mapTileType;
 
     if (isCurrentTileRoad) {
       // If current is road, then check if we need to change the sprite or just rotate it.
@@ -171,75 +274,77 @@ class MapComponent extends PositionComponent {
           isBottomTileRoad) {
         // When we are surrounded by roads it means we are on a junction
         sprite = _tilesToSprite[TileType.roadJunction]!;
+        tileType = TileType.roadJunction;
       } else if (isLeftTileRoad && isTopTileRoad) {
         // If left tile is road, and top also is, then we are in a turn
         sprite = _tilesToSprite[TileType.roadTurn]!;
         angle = 180;
+        tileType = TileType.roadTurn;
       } else if (isLeftTileRoad && isBottomTileRoad) {
         // If left tile is road, and bottom also is, then we are in a turn
         sprite = _tilesToSprite[TileType.roadTurn]!;
         angle = 90;
+        tileType = TileType.roadTurn;
       } else if (isRightTileRoad && isTopTileRoad) {
         // If right tile is road, and top also is, then we are in a turn
         sprite = _tilesToSprite[TileType.roadTurn]!;
         angle = 270;
+        tileType = TileType.roadTurn;
       } else if (isRightTileRoad && isBottomTileRoad) {
         // If right tile is road, and bottom also is, then we are in a turn
         sprite = _tilesToSprite[TileType.roadTurn]!;
         angle = 0;
+        tileType = TileType.roadTurn;
       } else if (isRightTileRoad || isLeftTileRoad) {
         // If left or right tile are roads, then we rotate the image 90 degrees
         angle = 90;
       }
     }
 
-    return Tile(sprite: sprite, rotationAngle: angle, type: mapTileType);
+    return Tile(sprite: sprite, rotationAngle: angle, type: tileType);
   }
 
-  void handleTap(int x, int y)
-  {
-    final isValidPosition = _validTowerPositions.any((pos) => pos.x == x && pos.y == y);
+  void handleTap(int x, int y) {
+    final isValidPosition = _validTowerPositions.any(
+      (pos) => pos.x == x && pos.y == y,
+    );
     final towerPosition = Vector2(x.toDouble(), y.toDouble());
-    final left = (x*_tileSize)+(_tileSize/2);
-    final top = (y*_tileSize)+(_tileSize/2);
+    final left = (x * _tileSize) + (_tileSize / 2);
+    final top = (y * _tileSize) + (_tileSize / 2);
 
     final towerGlobalPosition = Vector2(left, top);
 
-    if(!isValidPosition)
-    {
+    if (!isValidPosition) {
       debugPrint('Invalid tower position');
       _showErrorEffect(towerGlobalPosition);
       return;
     }
-    if (_occupiedTowerPositions.contains(towerPosition))
-    {
+    if (_occupiedTowerPositions.contains(towerPosition)) {
       debugPrint('Tower position already ocuppied');
       _showErrorEffect(towerGlobalPosition);
       return;
     }
-    final attributes = TowerAttributes(
-      damageModifier: 1.0,
-      reachModifier: 1.0,
-    );
-    
-    final tower = TowerComponent(
-      mapPos: towerPosition,
-      towerType: '',
-      tier: 1,
-      range: 1,
-      damage: 1,
-      attributes: attributes,
-    )
-      ..anchor=Anchor.center
-      ..position=towerGlobalPosition
-      ..sprite = towerSprite
-      ..size = Vector2.all(_tileSize);
+    final attributes = TowerAttributes(damageModifier: 1.0, reachModifier: 1.0);
+
+    final tower =
+        TowerComponent(
+            mapPos: towerPosition,
+            towerType: '',
+            tier: 1,
+            range: 1,
+            damage: 1,
+            attributes: attributes,
+          )
+          ..anchor = Anchor.center
+          ..position = towerGlobalPosition
+          ..sprite = towerSprite
+          ..size = Vector2.all(_tileSize);
     add(tower);
     _occupiedTowerPositions.add(towerPosition);
     _showSuccessEffect(towerGlobalPosition);
   }
 
-   Future<void> _showSuccessEffect(Vector2 position) async {
+  Future<void> _showSuccessEffect(Vector2 position) async {
     final effect = CircleComponent(
       radius: 20,
       position: position,
--- lib/game/enemy_component/enemy_component.dart
+++ lib/game/enemy_component/enemy_component.dart
@@ -1,30 +1,158 @@
 import 'package:flame/components.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_towerdefense_game/game/component/health_bar_component.dart';
 
-enum EnemyType {
-  type1, 
-  type2,
-  }
+enum EnemyType { type1, type2, type3 }
+
 class EnemyComponent extends SpriteComponent {
   late final int health;
   late final int speed;
-  late final List<Vector2> way;
+
+  late final List<List<int>> path;
+  late final Vector2 startPos;
+  late final List<List<SpriteComponent>> tiles;
+  final void Function()? onReachedEnd;
+  late Vector2 _pos;
+  late HealthBarComponent healthBar;
 
   EnemyComponent({
     required this.health,
     required this.speed,
-    required this.way,
+    required this.path,
+    required this.startPos,
+    required this.tiles,
+    this.onReachedEnd,
   });
 
-  EnemyComponent.build(EnemyType type, this.way) {
+  EnemyComponent.build({
+    required EnemyType type,
+    required this.path,
+    required this.startPos,
+    required this.tiles,
+    this.onReachedEnd,
+  }) {
+    _pos = startPos;
     switch (type) {
       case EnemyType.type1:
         health = 100;
-        speed = 15;
+        // enemy movement may break if speed is set too high (it broke with 100 speed)
+        speed = 50;
         break;
       case EnemyType.type2:
         health = 200;
-        speed = 5;
+        speed = 30;
+        break;
+      case EnemyType.type3:
+        health = 75;
+        speed = 80;
         break;
     }
+  }
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    final healthBarSize = Vector2(20, 4);
+    healthBar =
+        HealthBarComponent(
+            maxHealth: health.toDouble(),
+            currentHealth: health.toDouble(),
+          )
+          ..size = healthBarSize
+          ..position = Vector2(
+            (width / 2) - (healthBarSize.x / 2),
+            -10,
+          ); // position above sprite
+
+    add(healthBar); // add as children
+  }
+
+  @visibleForTesting
+  Vector2? nextPoint(Vector2 pos) {
+    final mapWidth = path[0].length;
+    final mapHeight = path.length;
+    final posX = pos.x.toInt();
+    final posY = pos.y.toInt();
+
+    // Calculate neighbour tiles according to this reference:
+    //     0       previousY     0
+    // previousX       x       nextX
+    //     0         nextY       0
+    final previousX = (posX - 1);
+    final nextX = (posX + 1);
+    final previousY = (posY - 1);
+    final nextY = (posY + 1);
+
+    // Check whether calculated tiles are inside the map or not
+    final hasTileInLeft = previousX >= 0 && previousX < mapWidth;
+    final hasTileInRight = nextX >= 0 && nextX < mapWidth;
+    final hasTileInTop = previousY >= 0 && previousY < mapHeight;
+    final hasTileInBottom = nextY >= 0 && nextY < mapHeight;
+
+    // Get each tile
+    // If left tile does exist, then get it.
+    final mapTileLeftType = hasTileInLeft ? path[posY][previousX] : null;
+    // If right tile does exist, then get it.
+    final mapTileRightType = hasTileInRight ? path[posY][nextX] : null;
+    // If top tile does exist, then get it.
+    final mapTileTopType = hasTileInTop ? path[previousY][posX] : null;
+    // If bottom tile does exist, then get it.
+    final mapTileBottomType = hasTileInBottom ? path[nextY][posX] : null;
+    // Greater than 0 means a road type
+    if ((mapTileBottomType ?? 0) > 0) {
+      return Vector2(posX.toDouble(), nextY.toDouble());
+    }
+    if ((mapTileRightType ?? 0) > 0) {
+      return Vector2(nextX.toDouble(), posY.toDouble());
+    }
+    if ((mapTileTopType ?? 0) > 0) {
+      return Vector2(posX.toDouble(), previousY.toDouble());
+    }
+
+    if ((mapTileLeftType ?? 0) > 0) {
+      return Vector2(previousX.toDouble(), posY.toDouble());
+    }
+    return null;
+  }
+
+  @override
+  void update(double dt) {
+    // TODO: move enemy outside playable map and remove it from component tree
+    var nextPos = nextPoint(_pos);
+
+    if (nextPos == null) {
+      // O inimigo chegou ao final
+      onReachedEnd?.call(); // Função que você vai criar para aplicar o dano
+      removeFromParent(); // Remove o inimigo da cena
+      return;
+    }
+
+    final nextTile = tiles[nextPos.y.toInt()][nextPos.x.toInt()];
+
+    var currentTileXDiff = nextTile.center.x - center.x;
+    var currentTileYDiff = nextTile.center.y - center.y;
+
+    // If we've reached the tile center we must find the next tile and move
+    // the enemy towards the next tile center
+    if (currentTileXDiff.abs() < 1 && currentTileYDiff.abs() < 1) {
+      // 0 means we've gone through it
+      // Any number above it will be treated as "not interacted"
+      //
+      // Junctions have a number of 3. In other words, they must be "walked on" twice
+      // in order to be counted as fully interacted with/finished
+      path[_pos.y.toInt()][_pos.x.toInt()] =
+          (path[_pos.y.toInt()][_pos.x.toInt()] / 3).floor();
+      _pos = nextPos;
+      // Fetch next point
+      nextPos = nextPoint(_pos);
+    }
+    final stepX = speed.toDouble();
+    final stepY = speed.toDouble();
+    var xDiff = nextTile.center.x - center.x;
+    var yDiff = nextTile.center.y - center.y;
+
+    position.x += dt * stepX * xDiff.sign;
+    position.y += dt * stepY * yDiff.sign;
+    super.update(dt);
   }
 }

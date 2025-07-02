43dda76c5ab69a3973692491e6f13a69dcdcf814
chelsearaulino <kaa.chelsea@gmail.com>
2025-07-02 16:40:48 -0300

    Fix: End Screen

--- lib/widgets/end_screen/game_over_screen_widget.dart
+++ lib/widgets/end_screen/game_over_screen_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class GameOverScreenWidget extends StatelessWidget {
-    final VoidCallback onMenuButtonClicked;
+  final VoidCallback onMenuButtonClicked;
 
   const GameOverScreenWidget({super.key, required this.onMenuButtonClicked});
 
@@ -9,22 +9,24 @@ class GameOverScreenWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-    body: SizedBox.expand(
-  child: Stack(
-    children: [
-      Center(child: Image.asset('assets/images/gameover1.png')),
-      Center(
-        child: Padding(
-          padding: EdgeInsets.only(top: MediaQuery.sizeOf(context).height*0.2),
-            child: ElevatedButton(
-            onPressed: onMenuButtonClicked,
-            child: const Text('Menu Principal'),
-          ),
-        )
+      body: SizedBox.expand(
+        child: Stack(
+          children: [
+            Center(child: Image.asset('assets/images/gameover1.png')),
+            Center(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.sizeOf(context).height * 0.2,
+                ),
+                child: ElevatedButton(
+                  onPressed: onMenuButtonClicked,
+                  child: const Text('Menu Principal'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ],
-  ),
-),
     );
   }
 }
--- lib/widgets/end_screen/victory_screen_widget.dart
+++ lib/widgets/end_screen/victory_screen_widget.dart
@@ -3,28 +3,30 @@ import 'package:flutter/material.dart';
 class VictoryScreenWidget extends StatelessWidget {
   final VoidCallback onMenuButtonClicked;
 
-  const VictoryScreenWidget({super.key, required this.onMenuButtonClicked });
+  const VictoryScreenWidget({super.key, required this.onMenuButtonClicked});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-    body: SizedBox.expand(
-  child: Stack(
-    children: [
-      Center(child: Image.asset('assets/images/victory1.png')),
-      Center(
-        child: Padding(
-          padding: EdgeInsets.only(top: MediaQuery.sizeOf(context).height*0.1),
-            child: ElevatedButton(
-            onPressed: onMenuButtonClicked,
-            child: const Text('Menu Principal'),
-          ),
-        )
+      body: SizedBox.expand(
+        child: Stack(
+          children: [
+            Center(child: Image.asset('assets/images/victory1.png')),
+            Center(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.sizeOf(context).height * 0.1,
+                ),
+                child: ElevatedButton(
+                  onPressed: onMenuButtonClicked,
+                  child: const Text('Menu Principal'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ],
-  ),
-),
     );
   }
 }

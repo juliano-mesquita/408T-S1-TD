2487f6ae93ce4c32baa982e027e57d6e80384e66
juliano-mesquita <juliano@mesquita.com.br>
2025-07-02 14:49:02 -0300

    fix: Broken pipeline due to broken dependency version (#42)
    * fix: Broken pipeline due to broken dependency version
    
    * fix: Player Balance Show Tests not correctly written

--- test/game/player/player_wallet_test.dart
+++ test/game/player/player_wallet_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_towerdefense_game/game/player/player_wallet.dart';
 
@@ -66,48 +65,6 @@ void main() {
     test('should return false when balance is less than price', () {
       final wallet = PlayerWallet(balance: 100);
       expect(wallet.enoughCoins(101), isFalse);
-    });
-  });
- group('Market Balance Display', () {
-    const playerBalance = 100;
-
-    late Widget widgetUnderTest;
-
-    setUp(() {
-      widgetUnderTest = MaterialApp(
-        home: Scaffold(
-          body: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/images/espelho_dindin.png',
-                width: 60.0,
-                height: 60.0,
-              ),
-              const SizedBox(width: 3.0),
-              Text(
-                '$playerBalance',
-                key: const Key('info.user.balance'),
-                style: const TextStyle(color: Colors.green, fontSize: 35),
-              ),
-            ],
-          ),
-        ),
-      );
-    });
-
-    testWidgets('should display the correct balance in the Text widget', (WidgetTester tester) async {
-      await tester.pumpWidget(widgetUnderTest);
-
-      // Find the Text widget by its key
-      final balanceTextFinder = find.byKey(const Key('info.user.balance'));
-
-      // Check that the Text widget is found
-      expect(balanceTextFinder, findsOneWidget);
-
-      // Get the Text widget and check the displayed value
-      final Text textWidget = tester.widget(balanceTextFinder);
-      expect(textWidget.data, '$playerBalance');
     });
   });
 }

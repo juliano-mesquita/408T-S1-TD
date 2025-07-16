e7f594b8a14fe2479dbfaea2bb276b92e19e2a26
Chelsea <kaa.chelsea@gmail.com>
2025-07-16 16:43:05 -0300

    Adicionado Itens do Mercado (#48)
    * WIP: Market Items
    
    * MArket Items Update

--- lib/game/market/market_inventory.dart
+++ lib/game/market/market_inventory.dart
@@ -25,32 +25,7 @@ class MarketInventory {
 
   static MarketInventory loadStatic() {
     return MarketInventory(
-      items: [
-        MarketItem(
-          id: 'tower1',
-          name: 'Torre Arqueira Indígena',
-          description: 'Atira flechas nos inimigos com alta precisão.',
-          price: 100,
-          type: MarketItemType.tower,
-          icon: 'assets/images/market/tower_arrow.png',
-        ),
-        MarketItem(
-          id: 'upgrade1',
-          name: 'Flechas de Pedra',
-          description: 'Aumenta o dano das torres em 20%.',
-          price: 75,
-          type: MarketItemType.upgrade,
-          icon: 'assets/images/market/upgrade_arrow.png',
-        ),
-        MarketItem(
-          id: 'resource1',
-          name: 'Bênção da Floresta',
-          description: 'Recupera a vida das torres lentamente.',
-          price: 50,
-          type: MarketItemType.resource,
-          icon: 'assets/images/market/forest_blessing.png',
-        ),
-      ],
+      items: [],
     );
   }
 

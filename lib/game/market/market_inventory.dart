64366b8bb9f4f4e7bf8e30dfd7b378b46fb686bb
chelsearaulino <kaa.chelsea@gmail.com>
2025-07-16 14:22:06 -0300

    MArket Items Update

--- lib/game/market/market_inventory.dart
+++ lib/game/market/market_inventory.dart
@@ -25,56 +25,7 @@ class MarketInventory {
 
   static MarketInventory loadStatic() {
     return MarketInventory(
-      items: [
-        MarketItem(
-          id: 'femea1',
-          name: 'Indígena Fêmea com Tocha',
-          description: 'Queima os inimigos com o elemento do fogo.',
-          price: 50,
-          type: MarketItemType.tower,
-          icon: 'assets/images/indios_garimpeiros/india_um.png',
-        ),
-        MarketItem(
-          id: 'macho2',
-          name: 'Indígena Macho com Lança',
-          description: 'Poderosa lança confeccionada em pedra maciça.',
-          price: 75,
-          type: MarketItemType.upgrade,
-          icon: 'assets/images/indios_garimpeiros/indio_dois.png',
-        ),
-        MarketItem(
-          id: 'femea2',
-          name: 'Indígena Fêmea com Lança',
-          description: 'Poderosa lança confeccionada em pedra maciça.',
-          price: 100,
-          type: MarketItemType.resource,
-          icon: 'assets/images/indios_garimpeiros/india_dois.png',
-        ),
-          MarketItem(
-          id: 'macho3',
-          name: 'Indígena Macho com  Arco e Flecha',
-          description: 'Atira flechas com precisão e a natural habilidade de caçador.',
-          price: 150,
-          type: MarketItemType.resource,
-          icon: 'assets/images/indios_garimpeiros/indio_tres.png',
-        ),
-  MarketItem(
-          id: 'miniindio',
-          name: 'Mini Indígena com Cenoura Gigante',
-          description: 'Super força com a cenoura gigante como arma.',
-          price: 175,
-          type: MarketItemType.resource,
-          icon: 'assets/images/indios_garimpeiros/mini_indio.png',
-        ),
-        MarketItem(
-          id: 'cacique',
-          name: 'Cacique',
-          description: 'Dono da Aldeia.',
-          price: 300,
-          type: MarketItemType.resource,
-          icon: 'assets/images/indios_garimpeiros/cacique.png',
-        ),
-      ],
+      items: [],
     );
   }
 
